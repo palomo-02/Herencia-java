301b939891576f40d9ea8de4434d07cfe527b962
Jose Manuel Palomo Zambrano <palomo.zambrano.jose.manuel@iescamas.es>
2025-01-21 17:13:14 +0100

    nuevo ej 2

---
+++ src/ej2/Publicacion.java
@@ -0,0 +1,18 @@
+package ej2;
+
+public class Publicacion {
+
+
+		protected String titulo;
+		protected int año;
+		
+		public Publicacion(String titulo, int año) {
+			this.titulo = titulo;
+			this.año = año;
+		}
+
+		
+		
+	
+
+}
